cbab6deaff42fb6597b172b0095208e18f130375
blockchainluffy <jainpunit1997@gmail.com>
2025-09-02 19:16:51 +0530

    added chain port

--- _container_scripts/configure_shuttermint.sh
+++ _container_scripts/configure_shuttermint.sh
@@ -12,8 +12,8 @@ ln -s /assets/genesis.json /data/chain/config/genesis.json
 sed -i "/^seeds =/c\seeds = \"3fbcca4faab2d056e6c5e5f3508165428de94f36@46.101.186.142:26656\"" $CFG
 sed -i "/^moniker =/c\moniker = \"${SHUTTERMINT_MONIKER}\"" $CFG
 sed -i "/^genesis_file =/c\genesis_file = \"/assets/genesis.json\"" $CFG
-sed -i "/^external_address =/c\external_address = \"${PUBLIC_IP}:27656\"" $CFG
-sed -i '/^\[p2p\]/,/^\[.*\]/ s/^laddr[[:space:]]*=.*/laddr = "tcp:\/\/0.0.0.0:27656"/' $CFG
+sed -i "/^external_address =/c\external_address = \"${PUBLIC_IP}:${SHUTTER_CHAIN_PORT}\"" $CFG
+sed -i '' "/^\[p2p\]/,/^\[.*\]/ s/^laddr[[:space:]]*=.*/laddr = \"tcp:\/\/0.0.0.0:${SHUTTER_CHAIN_PORT}\"/" $CFG
 sed -i "/^addr_book_strict =/c\addr_book_strict = true" $CFG
 sed -i "/^pex =/c\pex = true" $CFG
 if [ "$METRICS_ENABLED" = "true" ]; then
